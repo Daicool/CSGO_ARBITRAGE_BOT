f3d620676cb5e53fff342d15685c8c5981d08afa
Daicool <89376394+Daicool@users.noreply.github.com>
2025-07-17 11:01:51 +0700

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -3,6 +3,7 @@ import time
 import nacl.encoding
 import nacl.signing
 import urllib.parse
+import base64  # Thêm import base64
 
 def get_dmarket_headers(public_key, private_key, method, endpoint, query_params="", body=""):
     timestamp = str(int(time.time()))
@@ -19,13 +20,10 @@ def get_dmarket_headers(public_key, private_key, method, endpoint, query_params=
         "X-Request-Sign": signature
     }
 
-def get_skinport_token(client_id, client_secret):
-    url = "https://api.skinport.com/v1/auth"
-    data = {
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "grant_type": "client_credentials"
+def get_skinport_headers(client_id, client_secret):
+    encoded_data = f"{client_id}:{client_secret}".encode()
+    auth_string = base64.b64encode(encoded_data).decode('utf-8')
+    return {
+        "Authorization": f"Basic {auth_string}",
+        "Accept-Encoding": "br"
     }
-    response = requests.post(url, data=data)
-    response.raise_for_status()
-    return response.json().get("access_token")
